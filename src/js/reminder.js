e2287eca321d756586bccd2ddc08a432d442bc2b
Oleh Tsap <olehtsap07@gmail.com>
2023-03-21 20:40:32 +0200

    розбирання ДОМ дерева, та події

--- src/js/reminder.js
+++ src/js/reminder.js
@@ -1,13 +1,24 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// function sum(params) {
+//     let total = 0;
+//     for (const i of params) {
+    //         total += i/params.length
+    //     }
+    //     return total;
+    
+    // }
+
+// console.log(sum(numbers));
 
 
-function sum(params) {
-    let total = 0;
-    for (const i of params) {
-        total += i/params.length
-    }
-    return total;
-
+const formEl = document.querySelector('.js-form');
+    
+formEl.addEventListener('submit', info);
+    
+    
+function info(event) {
+    event.preventDefault();
+    console.dir(event.target[2].value);
 }
-
-console.log(sum(numbers));
