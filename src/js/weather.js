0dbd05d1bf53c06cc7cf9cc48def76bf0e10f220
Oleh Tsap <olehtsap07@gmail.com>
2023-03-28 20:35:26 +0300

    закінчив завдання з погодою

--- src/js/weather.js
+++ src/js/weather.js
@@ -2,14 +2,19 @@ import weatherTpl from '../templates/weather.hbs';
 
 const refs = {
     form: document.querySelector('.js-form'),
+    card: document.querySelector('.js-card-container'),
 }
 
 refs.form.addEventListener('submit', searchWeather);
 
 function weather(city) {
-    return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=40207e285e43c5b8e49ba7f2599cdd4b`)
+    return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=40207e285e43c5b8e49ba7f2599cdd4b&units=metric`)
         .then(data => data.json())
-        .then(data => console.log(data))
+    .then(weather => {
+            const markup = weatherTpl(weather);
+
+                refs.card.innerHTML = markup;
+            })
 }
 
 function searchWeather(event) {
